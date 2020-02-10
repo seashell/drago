25d61c6745052f20007b1ca14c44a5def4a1a837
matheus <winter.matheus@gmail.com>
2020-02-10 11:40:08 +0100

    Improve reconciliation logic and error handling

--- pkg/agent/client/client.go
+++ pkg/agent/client/client.go
@@ -18,6 +18,7 @@ type client struct {
 	httpClient *resty.Client
 	wgConf     *wg.Conf
 	wgIface    int
+	node	   *Node
 }
 
 type ClientConfig struct {
@@ -26,6 +27,7 @@ type ClientConfig struct {
 	ServerAddr   string `mapstructure:"server_addr"`
 	WgKey        string `mapstructure:"wg_key"`
 	Jwt          string `mapstructure:"jwt"`
+	DataDir		 string `mapstructure:"data_dir"`
 }
 
 type Client interface {
@@ -36,17 +38,29 @@ func New(c ClientConfig) (*client, error) {
 
 	wgIface, isNew, err := wg.New("wg0")
 	if err != nil {
-		fmt.Println(err)
+		return nil, fmt.Errorf("%v",err)
 	}
 
 	if isNew {
-		fmt.Println("New Wireguard interface create")
+		fmt.Println("New Wireguard interface create") //???
 	}
 
-	dat, err := ioutil.ReadFile("./wg0.conf")
+	dat, err := ioutil.ReadFile(c.DataDir + "/wg0.conf")
 	if err != nil {
-		fmt.Println("Error reading Wireguard configuration file")
+		fmt.Println("Error reading Wireguard config file: ",err)
 	}
+
+	n := &Node{}
+	node, err := ioutil.ReadFile(c.DataDir + "/wg0.json")
+	if err != nil {
+		fmt.Println("Error reading node JSON file: ",err)
+	} else {
+		err = json.Unmarshal(node, n)
+		if err != nil {
+			fmt.Println("Error unmarshalling node JSON: ",err)
+		}
+	}
+
 
 	// If a key already exists, use it. Otherwise generate a new one.
 	key := c.WgKey
@@ -61,11 +75,13 @@ func New(c ClientConfig) (*client, error) {
 		wgIface:    wgIface,
 		wgConf:     wg.Parse(dat),
 		httpClient: resty.New(),
+		node:		n,
 	}, nil
 
 }
 
-func (c *client) PollConfigServer() *Node {
+
+func (c *client) PollConfigServer() (*Node, error) {
 
 	url := "http://" + c.config.ServerAddr + "/api/v1/node"
 
@@ -81,55 +97,63 @@ func (c *client) PollConfigServer() *Node {
 		Post(url)
 
 	if err != nil {
-		fmt.Println(err)
+		return nil, fmt.Errorf("Error polling server: %v",err)
 	}
 
 	n := &Node{}
 	err = json.Unmarshal(resp.Body(), n)
 	n.Interface.PrivateKey = c.config.WgKey
 
-	return n
+	return n, nil
 }
 
 func (c *client) Reconcile(n *Node) error {
 
-	path := "./wg0.conf"
+	if n.Interface.Address != c.node.Interface.Address {
 
-	if err := templateToFile(tmpl, path, n); err != nil {
-		fmt.Println("Error writing Wireguard config to file")
+		path := c.config.DataDir+"/wg0.json"
+		buf, err := json.Marshal(n)
+		if err != nil {
+			return fmt.Errorf("Error formatting JSON: %v",err)
+		}
+		if err := ioutil.WriteFile(path, buf, 0600); err != nil {
+			return fmt.Errorf("Error writing node JSON to file: %v",err)
+		}
+
+		_, ifaceAddr, err := net.ParseCIDR(n.Interface.Address)
+		if err != nil {
+			return fmt.Errorf("Error parsing interface CIDR: %v",err)
+		}
+
+		if err = ip.SetAddress(c.wgIface, ifaceAddr); err != nil{
+			return fmt.Errorf("Error setting interface IP address: %v",err)
+		}	
+
+		c.node = n
 	}
 
 	conf, err := templateToString(tmpl, n)
 	if err != nil {
-		fmt.Println("Error converting Wireguard config to string")
+		return fmt.Errorf("Error converting Wireguard config to string: %v",err)
 	}
 
 	newConf := wg.Parse([]byte(conf))
+	if equal := newConf.Equal(c.wgConf); !equal {
 
-	if newConf.Equal(c.wgConf) {
-		fmt.Println("Configuration is still the same. No need for reconciliation.")
-		return nil
+		path := c.config.DataDir+"/wg0.conf"
+		if err := templateToFile(tmpl, path, n); err != nil {
+			return fmt.Errorf("Error writing Wireguard config to file: %v",err)
+		}
+		
+		if err = wg.SetConf("wg0", path); err != nil{
+			return fmt.Errorf("Error applying Wiregard conf: %v",err)
+		}
+
+		c.wgConf = newConf
 	}
-
-	fmt.Println("Reconciling Wireguard configuration state.")
-	c.wgConf = newConf
-
-	iface,_,err := wg.New("wg0")
-	if err != nil {
-		return err
-	}
-
-	_, ifaceAddr, err := net.ParseCIDR(n.Interface.Address)
-	if err != nil {
-		return err
-	}
-
-	if err = ip.SetAddress(iface, ifaceAddr); err != nil{
-		return err 
-	}
-
-	if err = wg.SetConf("wg0", path); err != nil{
-		return err 
+	
+	if err = ip.Set(c.wgIface, true); err != nil {
+		return fmt.Errorf("Error bringing interface up: %v",err)
 	}
 
 	return nil
@@ -137,11 +161,13 @@ func (c *client) Reconcile(n *Node) error {
 }
 
 func (c *client) Run() {
-
 	go func() {
 		for {
 			time.Sleep(time.Duration(c.config.SyncInterval) * time.Second)
-			n := c.PollConfigServer()
+			n, err := c.PollConfigServer()
+			if err != nil {
+				fmt.Println(err.Error())
+			}
 			if err := c.Reconcile(n); err != nil{
 				fmt.Println(err.Error())
 			}
