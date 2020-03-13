4272cffdc3dd19cb3ddfb514fcb753a57b6443e9
mkwinter <mkwinter@seashell.sh>
2020-03-13 11:50:13 +0100

    Add dir and file configuration handlers to root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -18,8 +18,11 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"io"
+	"strings"
+	"path/filepath"
 
-	homedir "github.com/mitchellh/go-homedir"
+	//homedir "github.com/mitchellh/go-homedir"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -52,8 +55,8 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.drago.yaml)")
-
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "","/etc/drago.d", "config file (default is /etc/drago.d)")
+	viper.SetDefault("config", "/etc/drago.d")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
@@ -63,28 +66,98 @@ func init() {
 func initConfig() {
 
 	viper.SetConfigType("yaml")
-
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		// Search config in home directory with name ".seashell" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".seashell")
-	}
-
 	viper.AutomaticEnv() // read in environment variables that match
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("==> Loaded configuration from ", viper.ConfigFileUsed())
-	} else {
-		fmt.Println("==> Error loading configuration: %v", err)
+	LoadConfig(cfgFile)
+}
+
+
+func LoadConfig(path string) (error) {
+
+	//check if dir or file
+	fi, err := os.Stat(path)
+
+	if err != nil {
+		return err
 	}
+
+	//handle dir
+	if fi.IsDir() {
+		return LoadConfigDir(path)
+	}
+
+	return LoadConfigFile(path)
+}
+
+func LoadConfigDir(dir string) (error) {
+	
+	f, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	if !fi.IsDir() {
+		return fmt.Errorf("configuration path must be a directory: %s", dir)
+	}
+
+	file := ""
+	err = nil
+	for err != io.EOF {
+		var fis []os.FileInfo
+		fis, err = f.Readdir(128)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		for _, fi := range fis {
+			// Ignore directories
+			if fi.IsDir() {
+				continue
+			}
+
+			// Only care about files that are valid to load.
+			name := fi.Name()
+			skip := true
+			if strings.HasSuffix(name, ".yml") {
+				skip = false
+			}
+			if skip || file != ""  {
+				continue
+			}
+
+			path := filepath.Join(dir, name)
+			file = path
+		}
+	}
+
+	// Fast-path if we have no files
+	if file == "" {
+		return nil
+	}
+
+	err = LoadConfigFile(file)
+	if err != nil {
+		return fmt.Errorf("Error loading %s: %s", f, err)
+	}
+
+	return nil
+}
+
+func LoadConfigFile(path string) (error) {
+
+	viper.SetConfigFile(path)
+	err := viper.ReadInConfig()
+
+	// If a config file is found, read it in.
+	if err == nil {
+			fmt.Println("==> Loaded configuration from ", viper.ConfigFileUsed())
+	} else {
+			fmt.Println("==> Error loading configuration: ", err)
+	}
+	return err
 }
