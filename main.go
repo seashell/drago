1c567ee47d26c9ddd726107e2052d3d24d7b41d1
Unknown <edufschmidt@gmail.com>
2020-05-22 10:30:47 -0300

    Update call to root command from main

--- main.go
+++ main.go
@@ -3,6 +3,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/seashell/drago/command"
 	log "github.com/sirupsen/logrus"
 )
@@ -16,5 +19,9 @@ func main() {
 
 	log.SetLevel(log.DebugLevel)
 
-	command.Execute()
+	rootCmd := command.NewRootCmd()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
