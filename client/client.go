febee3c9d086167b05b2c3796cdb809d7de68d5d
matheus <mkwinter@seashell.sh>
2020-06-29 12:32:56 -0300

    Add attempt to configure host with local settings before syncing

--- client/client.go
+++ client/client.go
@@ -54,6 +54,8 @@ func New(c Config) (*Client, error) {
 		return nil,err
 	}
 
+
+
 	return &Client{
 		config:     c,
 		niCtrl: 	n,
@@ -62,7 +64,18 @@ func New(c Config) (*Client, error) {
 	}, nil
 }
 
-func (c *Client) Run() {
+func (c *Client) Run() {	
+	fmt.Println("Loading and applying current local settings ...")
+	ls, err := c.stateDB.GetHostSettings()
+				if err != nil {
+					fmt.Println("warning, failed to parse settings from DB: ",err)
+				}
+	if err := c.niCtrl.Update(c.fromApiSettingsToNic(ls));err != nil {
+		fmt.Println("warning, failed to update network interfaces: ",err)	
+	} else {
+		fmt.Println("Done applying current local settings ...")
+	}
+
 	fmt.Println("Syncing with remote servers every ", c.config.SyncInterval)
 	go func() {
 		for {
@@ -86,92 +99,7 @@ func (c *Client) Run() {
 				//If target remote settings != local settings, apply remote settings
 				if ! reflect.DeepEqual(ts,ls) {
 					fmt.Println("Started updating network interfaces ...")
-					targetSettings := []nic.Settings{}
-					for _,ni := range ts.NetworkInterfaces {
-						//Parse WG settings
-
-						// peers
-						var wgPeers []wgtypes.PeerConfig
-						for _,peer := range ts.WireguardPeers {
-							if *ni.Name == *peer.Interface {								
-								//Key
-								var pub wgtypes.Key
-								var err error
-								if peer.PublicKey != nil {
-									pub,err = wgtypes.ParseKey(*peer.PublicKey)
-									if err != nil { 
-										fmt.Println("warning: failed to parse public key: ",err)
-									}
-								}
-
-								//AllowedIPs
-								var allowedIPs []net.IPNet
-								for _,ip := range peer.AllowedIps {
-									_,allowedIP,err := net.ParseCIDR(ip)
-									if err != nil {
-										fmt.Println("warning: failed to parse IP CIDR: ",err)
-									}
-									allowedIPs = append(allowedIPs, *allowedIP)
-								}
-
-								//PersistentKeepalive
-								var persistentKeepalive *time.Duration
-								if peer.PersistentKeepalive != nil {
-									t := time.Duration(*peer.PersistentKeepalive) * time.Second
-									persistentKeepalive = &t
-								}
-
-								//Endpoint
-								var endpoint *net.UDPAddr
-								if peer.Address != nil {
-									p,_ := strconv.Atoi(*peer.Port)
-									endpoint = &net.UDPAddr{
-										IP: net.ParseIP(*peer.Address),
-										Port: p,
-									}
-								}
-
-								wgPeer := wgtypes.PeerConfig{
-									Remove: 						false,
-									UpdateOnly: 					false,
-									ReplaceAllowedIPs: 				true,
-									PresharedKey: 					nil,
-									PublicKey: 						pub,
-									AllowedIPs: 					allowedIPs,
-									Endpoint:						endpoint,
-									PersistentKeepaliveInterval:	persistentKeepalive,
-								}
-								wgPeers = append(wgPeers,wgPeer)
-							}							
-						}
-
-						//ListenPort
-						var listenPort *int
-						if ni.ListenPort != nil {
-							lp,_ := strconv.Atoi(*ni.ListenPort)
-							listenPort = &lp
-						}
-
-						wgConfig := &wgtypes.Config{
-							PrivateKey: 	c.niCtrl.GetWgPrivateKey(),
-							ListenPort:		listenPort, 
-							ReplacePeers:	true,
-							Peers:			wgPeers,
-						}
-
-						//Parse link device settings
-						//Address 
-						addr,err := netlink.ParseAddr(*ni.Address) 
-						if err != nil {
-							fmt.Println("failed to parse IP address:", err)
-						}
-						targetSettings = append(targetSettings, nic.Settings{
-							Name: 		ni.Name,
-							Address:	addr,
-							Wireguard: 	wgConfig,
-						})
-					}
-					if err := c.niCtrl.Update(targetSettings);err != nil {
+					if err := c.niCtrl.Update(c.fromApiSettingsToNic(ts));err != nil {
 						//If not sucessfull,  do not persist remote settings
 						fmt.Println("warning, failed to update network interfaces: ",err)	
 					} else {
@@ -188,4 +116,93 @@ func (c *Client) Run() {
 		}
 	}()
  	
+}
+
+func (c *Client) fromApiSettingsToNic(as *api.HostSettings) ([]nic.Settings) {
+	ts := []nic.Settings{}
+	for _,ni := range as.NetworkInterfaces {
+		//Parse WG settings
+
+		// peers
+		var wgPeers []wgtypes.PeerConfig
+		for _,peer := range as.WireguardPeers {
+			if *ni.Name == *peer.Interface {								
+				//Key
+				var pub wgtypes.Key
+				var err error
+				if peer.PublicKey != nil {
+					pub,err = wgtypes.ParseKey(*peer.PublicKey)
+					if err != nil { 
+						fmt.Println("warning: failed to parse public key: ",err)
+					}
+				}
+
+				//AllowedIPs
+				var allowedIPs []net.IPNet
+				for _,ip := range peer.AllowedIps {
+					_,allowedIP,err := net.ParseCIDR(ip)
+					if err != nil {
+						fmt.Println("warning: failed to parse IP CIDR: ",err)
+					}
+					allowedIPs = append(allowedIPs, *allowedIP)
+				}
+
+				//PersistentKeepalive
+				var persistentKeepalive *time.Duration
+				if peer.PersistentKeepalive != nil {
+					t := time.Duration(*peer.PersistentKeepalive) * time.Second
+					persistentKeepalive = &t
+				}
+
+				//Endpoint
+				var endpoint *net.UDPAddr
+				if peer.Address != nil {
+					p,_ := strconv.Atoi(*peer.Port)
+					endpoint = &net.UDPAddr{
+						IP: net.ParseIP(*peer.Address),
+						Port: p,
+					}
+				}
+
+				wgPeer := wgtypes.PeerConfig{
+					Remove: 						false,
+					UpdateOnly: 					false,
+					ReplaceAllowedIPs: 				true,
+					PresharedKey: 					nil,
+					PublicKey: 						pub,
+					AllowedIPs: 					allowedIPs,
+					Endpoint:						endpoint,
+					PersistentKeepaliveInterval:	persistentKeepalive,
+				}
+				wgPeers = append(wgPeers,wgPeer)
+			}							
+		}
+
+		//ListenPort
+		var listenPort *int
+		if ni.ListenPort != nil {
+			lp,_ := strconv.Atoi(*ni.ListenPort)
+			listenPort = &lp
+		}
+
+		wgConfig := &wgtypes.Config{
+			PrivateKey: 	c.niCtrl.GetWgPrivateKey(),
+			ListenPort:		listenPort, 
+			ReplacePeers:	true,
+			Peers:			wgPeers,
+		}
+
+		//Parse link device settings
+		//Address 
+		addr,err := netlink.ParseAddr(*ni.Address) 
+		if err != nil {
+			fmt.Println("failed to parse IP address:", err)
+		}
+		ts = append(ts, nic.Settings{
+			Name: 		ni.Name,
+			Address:	addr,
+			Wireguard: 	wgConfig,
+		})
+	}
+	return ts
 }
