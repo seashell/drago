ebb447108a6bded340cbd53aff992b62d4f97b92
Eduardo Schmidt <edufschmidt@gmail.com>
2021-08-01 22:31:37 +0200

    Set API URL correctly if running in development mode

--- ui/src/environment.js
+++ ui/src/environment.js
@@ -1,3 +1,5 @@
 export const DEBUG = process.env.REACT_APP_DEBUG || false
 
-export const REST_API_URL = process.env.REACT_APP_REST_API_URL || ''
+const defaultApiURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : ''
+
+export const REST_API_URL = process.env.REACT_APP_REST_API_URL || defaultApiURL
